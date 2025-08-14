031329fe807858ad21ab74ac8779add164e47818
Lester López <llopez-2022064@kinal.edu.gt>
2025-08-14 12:24:37 -0600

    fix: correct import path for app module in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { initServer } from "./configs/app";
-import { connect } from "./configs/mongo";
+import { initServer } from "./configs/app.js";
+import { connect } from "./configs/mongo.js";
 
 initServer()
 connect()
