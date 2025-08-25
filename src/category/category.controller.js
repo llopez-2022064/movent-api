f6fb5e88969772cf19861b418f5edbb95e5718a8
Lester López <llopez-2022064@kinal.edu.gt>
2025-08-25 17:28:22 -0600

    feat: Implementation of the endpoint to update categories

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -27,3 +27,34 @@ export const createCategory = async (req, res) => {
         return res.status(500).send({ msg: 'Error creating category' })
     }
 }
+
+export const updateCategory = async (req, res) => {
+    try {
+        let data = req.body
+        let userId = req.user.id
+        let { id } = req.params
+
+        let category = await Category.findOne({ _id: id })
+        if (!category) return res.status(404).send({ msg: 'Category not found' })
+
+        let user = await User.findOne({ _id: userId })
+        if (!user) return res.status(404).send({ msg: 'User not found' })
+
+        let { valid, field } = validateFieldIsEmpty(data, ['name'])
+        if (!valid) return res.status(400).send({ msg: '${field} is required' })
+
+        let existsCategory = await Category.findOne({ name: data.name, user: userId, _id: { $ne: id} })
+        if (existsCategory) return res.status(400).send({ msg: 'There is already a category with the same name' })
+
+        let categoryUpdate = await Category.findOneAndUpdate(
+            { _id: id },
+            data,
+            { new: true }
+        )
+
+        return res.status(200).send({ msg: categoryUpdate })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ msg: 'Error updating category' })
+    }
+}
--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -7,5 +7,6 @@ import { createCategory, updateCategory } from './category.controller.js'
 const api = Router()
 
 api.post('/', [validateJwt], createCategory)
+api.put('/:id', [validateJwt], updateCategory)
 
 export default api
