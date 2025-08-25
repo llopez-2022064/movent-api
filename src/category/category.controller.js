1f566956680dd2525baa5a4c92d548ae467ed0ee
Lester López <llopez-2022064@kinal.edu.gt>
2025-08-25 17:59:38 -0600

    refactor: remove redundant user existence check

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -1,16 +1,12 @@
 'use strict'
 
 import Category from './category.model.js'
-import User from '../user/user.model.js'
 import { validateFieldIsEmpty } from '../utils/validator.js'
 
 export const createCategory = async (req, res) => {
     try {
         let data = req.body
         let { id } = req.user
-
-        let user = await User.findOne({ _id: id })
-        if (!user) return res.status(404).send({ msg: 'User not found' })
 
         let existsCategory = await Category.findOne({ name: data.name, user: id })
         if (existsCategory) return res.status(400).send({ msg: 'There is already a category with that name.' })
@@ -37,9 +33,6 @@ export const updateCategory = async (req, res) => {
         let category = await Category.findOne({ _id: id })
         if (!category) return res.status(404).send({ msg: 'Category not found' })
 
-        let user = await User.findOne({ _id: userId })
-        if (!user) return res.status(404).send({ msg: 'User not found' })
-
         let { valid, field } = validateFieldIsEmpty(data, ['name'])
         if (!valid) return res.status(400).send({ msg: '${field} is required' })
 
@@ -62,9 +55,6 @@ export const updateCategory = async (req, res) => {
 export const getCategories = async (req, res) => {
     try {
         let user = req.user
-
-        let existsUser = await User.findOne({ _id: user.id })
-        if (!existsUser) return res.status(404).send({ msg: 'User not found' })
 
         let categories = await Category.find({ user: user.id }).populate('user', 'name email')
         if (categories.length === 0) return res.status(404).send({ msg: 'There are no categories' })
