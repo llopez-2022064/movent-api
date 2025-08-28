52bccfa7e20702535bb7127ad6466bd350ba9214
Lester López <llopez-2022064@kinal.edu.gt>
2025-08-28 12:17:31 -0600

    feat: Account update complete, validating input fields

--- src/account/account.controller.js
+++ src/account/account.controller.js
@@ -23,3 +23,35 @@ export const addAccount = async (req, res) => {
         return res.status(500).send({ msg: 'Error adding account' })
     }
 }
+
+export const updateAccount = async (req, res) => {
+    try {
+        let data = req.body
+        let user = req.user
+        let { id } = req.params
+
+        let account = await Account.findOne({ _id: id })
+        if (!account) return res.status(404).send({ msg: 'The account does not exist' })
+
+        let existsAccountName = await Account.findOne({ name: data.name, _id: { $ne: id } })
+        if (existsAccountName) return res.status(400).send({ msg: 'There is already an account with that name' })
+
+        if (data.openingBalance !== undefined && !isNumber(data.openingBalance)) {
+            return res.status(400).send({ msg: 'The amount entered is incorrect' });
+        }
+
+        let { valid, field } = validateFieldIsEmpty(data, ['name', 'category'])
+        if (!valid) return res.status(400).send({ msg: `${field} is required` })
+
+        let accountUpdate = await Account.findByIdAndUpdate(
+            { _id: id },
+            data,
+            { new: true }
+        )
+
+        return res.status(200).send({ msg: 'The account has been successfully updated', accountUpdate })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ msg: 'Error updating an account' })
+    }
+}
--- src/account/account.routes.js
+++ src/account/account.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { addAccount } from './account.controller.js'
+import { addAccount, updateAccount } from './account.controller.js'
 import { validateJwt } from '../middlewares/validate-jwt.js'
 
 const api = Router()
 
 api.post('/', [validateJwt], addAccount)
+api.patch('/:id', [validateJwt], updateAccount)
 
 export default api
