e82599c84c6870e8201781ac8ba9c82b8fdd4c2d
Lester López <llopez-2022064@kinal.edu.gt>
2025-08-29 10:08:51 -0600

    feat: full functionality to update expenses

--- src/expense/expense.controller.js
+++ src/expense/expense.controller.js
@@ -34,3 +34,60 @@ export const createExpense = async (req, res) => {
         return res.status(500).send({ msg: 'Error adding an expense' })
     }
 }
+
+export const editExpense = async (req, res) => {
+    try {
+        let { id } = req.params
+        let data = req.body
+        let user = req.user
+
+        let expense = await Expense.findOne({ _id: id, user: user.id })
+        if (!expense) return res.status(404).send({ msg: 'Expense not found' })
+
+        let { valid, field } = validateFieldIsEmpty(data, ['name', 'amount', 'account'])
+        if (!valid) return res.status(400).send({ msg: `${field} is required` })
+
+        if (data.amount <= 0) return res.status(400).send({ msg: 'The amount must be greater than zero' })
+        if (data.amount !== undefined && !isNumber(data.amount)) {
+            return res.status(400).send({ msg: 'The amount entered is incorrect.' })
+        }
+
+        let newAccount = await Account.findOne({ _id: data.account, user: user.id })
+        if (!newAccount) return res.status(404).send({ msg: 'Account not found' })
+
+        //Devolver saldo a la cuenta anterior
+        let previousAccount = await Account.findOne({ _id: expense.account, user: user.id })
+        if (previousAccount) {
+            previousAccount.openingBalance += expense.amount
+            await previousAccount.save()
+        }
+
+        if (newAccount.openingBalance < data.amount) {
+            return res.status(400).send({ msg: 'Insufficient balance in account' })
+        }
+
+        //Descontar de la nueva cuenta
+        newAccount.openingBalance -= data.amount
+        await newAccount.save()
+
+        let expenseUpdate = await Expense.findByIdAndUpdate(
+            { _id: id },
+            data,
+            { new: true }
+        )
+
+        if (!expenseUpdate) return res.status(404).send({ msg: 'Expense not found and not updated' })
+
+        return res.status(200).send({
+            msg: 'Expense updated successfully',
+            expense: expenseUpdate,
+            accounts: {
+                previous: previousAccount,
+                current: newAccount
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ msg: 'Error when editing the expense' })
+    }
+}
