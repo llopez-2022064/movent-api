92be1650ab049316301856725a93c7f7563f1a9f
Lester López <llopez-2022064@kinal.edu.gt>
2025-09-18 12:14:16 -0600

    feat: obtain the total monthly income

--- src/income/income.routes.js
+++ src/income/income.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateJwt } from '../middlewares/validate-jwt.js'
-import { addIncome, deleteIncome, getIncomes, updateIncome } from "./income.controller.js";
+import { addIncome, deleteIncome, getIncomes, getMonthlyIncome, updateIncome } from "./income.controller.js";
 
 const api = Router()
 
@@ -8,5 +8,6 @@ api.post('/', [validateJwt], addIncome)
 api.put('/:id', [validateJwt], updateIncome)
 api.get('/', [validateJwt], getIncomes)
 api.delete('/:id', [validateJwt], deleteIncome)
+api.get('/monthly-income', [validateJwt], getMonthlyIncome)
 
 export default api
